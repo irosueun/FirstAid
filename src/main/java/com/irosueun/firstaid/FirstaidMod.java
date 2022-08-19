fa39e5396afd81fb9de5a36e58c22eba22a7052d
peaceheis <69596656+peaceheis@users.noreply.github.com>
2022-08-18 20:45:34 -0500

    Thing have been done

--- src/main/java/com/irosueun/firstaid/FirstaidMod.java
+++ src/main/java/com/irosueun/firstaid/FirstaidMod.java
@@ -13,7 +13,11 @@ import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.BiomePlacementModifier;
 import net.minecraft.world.gen.decorator.CountPlacementModifier;
+import net.minecraft.world.gen.decorator.HeightRangePlacementModifier;
+import net.minecraft.world.gen.decorator.InSquarePlacementModifier;
 import net.minecraft.world.gen.feature.*;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
@@ -35,11 +39,10 @@ public class FirstaidMod implements ModInitializer {
 	public static final Block HEART_CRYSTAL_SHARD_ORE = new ExperienceDroppingBlock(QuiltBlockSettings.of(Material.STONE).strength(3.0F, 3.0F).requiresTool(), UniformIntProvider.create(10, 15));
 	public static final Block DEEPSLATE_HEART_CRYSTAL_SHARD_ORE = new ExperienceDroppingBlock(QuiltBlockSettings.of(Material.STONE).strength(4.5F, 3.0F).requiresTool(), UniformIntProvider.create(10, 15));
 
-	public static final ConfiguredFeature<?, ?> CONFIGURED_HEART_CRYSTAL_SHARD_FEATURE = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MODID, "heart_crystal_shard_configured"), new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(List.of(OreFeatureConfig.createTarget(STONE_ORE_REPLACEABLES, HEART_CRYSTAL_SHARD_ORE.getDefaultState()), OreFeatureConfig.createTarget(DEEPSLATE_ORE_REPLACEABLES, DEEPSLATE_HEART_CRYSTAL_SHARD_ORE.getDefaultState())), 1)));
-	public static final PlacedFeature PLACED_HEART_CRYSTAL_SHARD_FEATURE = new PlacedFeature(Holder.createDirect(CONFIGURED_HEART_CRYSTAL_SHARD_FEATURE), List.of(CountPlacementModifier.create(20)));
+	public static final ConfiguredFeature<?, ?> CONFIGURED_HEART_CRYSTAL_SHARD_FEATURE = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MODID, "heart_crystal_shard_configured"), new ConfiguredFeature(Feature.ORE, new OreFeatureConfig(List.of(OreFeatureConfig.createTarget(DEEPSLATE_ORE_REPLACEABLES, DEEPSLATE_HEART_CRYSTAL_SHARD_ORE.getDefaultState()), OreFeatureConfig.createTarget(STONE_ORE_REPLACEABLES, HEART_CRYSTAL_SHARD_ORE.getDefaultState())), 3)));
+	public static final PlacedFeature PLACED_HEART_CRYSTAL_SHARD_FEATURE = new PlacedFeature(Holder.createDirect(CONFIGURED_HEART_CRYSTAL_SHARD_FEATURE), List.of(CountPlacementModifier.create(20), InSquarePlacementModifier.getInstance(), HeightRangePlacementModifier.createUniform(YOffset.getBottom(), YOffset.fixed(64)), BiomePlacementModifier.getInstance()));
 
 	public static final Item HEART_CRYSTAL_SHARD = new Item(new QuiltItemSettings().group(ItemGroup.MISC));
-
 
 	@Override
 	public void onInitialize(ModContainer mod) {
