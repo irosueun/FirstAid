fd6f37c933c258578e982b46aa0450e07fcede96
iro <irosueun@gmail.com>
2022-08-19 23:41:06 -0300

    modid updaet

--- src/main/java/com/irosueun/firstaid/mixin/TitleScreenMixin.java
+++ src/main/java/com/irosueun/firstaid/mixin/TitleScreenMixin.java
@@ -1,5 +1,6 @@
 package com.irosueun.firstaid.mixin;
 
+import com.irosueun.firstaid.FirstaidMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		FirstaidMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
